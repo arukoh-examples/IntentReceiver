bce97fcddc39657a8842411aa6a7265288e06e3b
arukoh <arukoh10@gmail.com>
2014-02-09 02:09:35 +0900

    クリックしたリンクURLを表示する

--- src/tk/arukoh/intentreceiver/MainActivity.java
+++ src/tk/arukoh/intentreceiver/MainActivity.java
@@ -1,8 +1,10 @@
 package tk.arukoh.intentreceiver;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		TextView textView1 = (TextView) findViewById(R.id.textView1);
+		textView1.setText("");
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		if (Intent.ACTION_VIEW.equals(action)) {
+			android.net.Uri uri = intent.getData();
+			textView1.setText(uri.toString());
+		}
 	}
 
 	@Override
